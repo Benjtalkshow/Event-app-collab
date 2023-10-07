93ca09b4a2eefe6b0c9c023e1e6dbcf3f148b98d
Sunday Emijere (Business Guest) <b-semijere@microsoft.com>
2023-10-08 00:22:07 +0100

    Feat: Added auth page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,37 @@
+import { Button } from '@/components/Button';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <main>
-      <h1>Next.js + Tailwind CSS</h1>
-      <p>
-        This is a starter template for a Next.js project with Tailwind CSS and
-        TypeScript.
-      </p>
+    <main className='md:flex justify-between h-screen '>
+      <div className='px-16 h-full bg-secondary text-white w-full space-y-8 md:w-1/2 flex flex-col justify-center items-start'>
+        <h1 className='font-bold text-4xl max-w-[250px]'>
+          Connect and Socialize
+        </h1>
+        <p>
+          All your events in one place. Plan, share connect and socialize with
+          all your cliques. All for free.
+        </p>
+        <div className='lg:w-[50%] w-full'>
+          <Button
+            className=' text-secondary w-full py-6 text-md'
+            onClick={() => router.push('/login')}
+          >
+            Get Started
+          </Button>
+        </div>
+      </div>
+      <div className='hidden relative md:flex md:w-1/2 md:justify-center md:items-center'>
+        <Image
+          src='/images/holding-smartphone-hand.png'
+          fill
+          alt='holding smartphone'
+          className=' object-cover w-full h-full'
+        />
+      </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/modules/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     fontFamily: {
-      inter: "var(--font-inter)",
-      montserrat: "var(--font-montserrat)",
+      inter: 'var(--font-inter)',
+      montserrat: 'var(--font-montserrat)',
     },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'login-bg': "url('/images/holding-smartphone-hand.png')",
       },
       colors: {
         primary: {
-          DEFAULT: "#FFC6BC",
-          peach: "#FFC6BC",
-          blue: "#D2F5FE",
-          orange: "#FFE0C4",
-          grey: "#4C4C4C",
+          DEFAULT: '#FFC6BC',
+          peach: '#FFC6BC',
+          blue: '#D2F5FE',
+          orange: '#FFE0C4',
+          grey: '#4C4C4C',
         },
         secondary: {
-          DEFAULT: "#3F3849",
+          DEFAULT: '#3F3849',
         },
       },
     },
